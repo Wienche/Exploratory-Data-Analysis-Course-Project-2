c90ea48dda7417a943c7086aabacefc56cfa00e9
Jiachang Xu <jiachanx@usc.edu>
2017-06-20 15:43:52 +0800

    Plot 3 Completed

--- plot3.R
+++ plot3.R
@@ -12,17 +12,16 @@ library(ggplot2)
 
 
 
-## Total emissions in Baltimore City
-baltimore <- NEI[which(NEI$fips == 24510),]
-baltimore$year <- factor(baltimore$year, levels=c('1999', '2002', '2005', '2008'))
+baltimore <- NEI[NEI$fips=="24510",]
 
+baltimore.aggregate <- aggregate(Emissions ~ year, baltimore, sum)
 
-
-## Plotting
-png("plot3.png", width=800, height=500, units='px')
-ggplot(data=baltimore, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
-    geom_boxplot(aes(fill = type)) + stat_boxplot(geom ='errorbar') +
-    ylab('Log of PM2.5 Emissions in Baltimore(tons)') + xlab('Year') + 
-    ggtitle('Total PM2.5 Emissions in Baltimore from 1999 to 2008') +
-    geom_jitter(alpha=0.10)
+png("./plot3.png", width=600, height=480, units="px")
+g <- ggplot(baltimore,aes(factor(year), Emissions, fill=type)) +
+    geom_bar(stat="identity") +
+    theme_bw() + guides(fill=FALSE)+
+    facet_grid(.~type, scales="free", space="free") + 
+    labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+    labs(title=expression("PM"[2.5]*" Emissions in Baltimore from 1999 to 2008 by Source Type"))
+print(g)
 dev.off()
