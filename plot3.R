685b29948658cc3e319d14bbbbf44009cface2cb
Jiachang Xu <jiachanx@usc.edu>
2017-06-20 14:49:24 +0800

    Plot 3 Completed

---
+++ plot3.R
@@ -0,0 +1,33 @@
+## This first line will likely take a few seconds. Be patient!
+if(!exists("NEI")){
+    NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+    SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
+## Transform
+NEI <- transform(NEI, SCC = factor(SCC))
+NEI <- transform(NEI, year = factor(year))
+NEI <- transform(NEI, type = factor(type))
+
+
+
+require(ggplot2)
+
+
+
+## Total emissions in Baltimore City
+baltimore <- NEI[which(NEI$fips == 24510),]
+baltimoreEmissions <- aggregate(Emissions ~ year + type, baltimore, sum)
+
+
+
+## Plotting
+png("./plot3.png")
+plot3 <- ggplot(baltimoreEmissions, aes(year, Emissions, color = type))
+plot3 <- plot3 + geom_line() +
+    xlab("Year") +
+    ylab("Total PM2.5 Emissions in Baltimore(tons)") +
+    ggtitle("Total PM2.5 Emissions in Baltimore from 1999 to 2008")
+print(plot3)
+dev.off()
